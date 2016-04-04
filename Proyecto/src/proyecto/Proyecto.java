03a93e6ee92d0120824a61fb372ad6d8abdd660b
fernando0989 <edgarss_15@hotmail.com>
2016-04-03 23:27:47 -0500

    nuevo

---
+++ Proyecto/src/proyecto/Proyecto.java
@@ -0,0 +1,11 @@
+
+package proyecto;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        Identificador l1= new Identificador(); 
+        l1.setVisible(true);
+    
+    }
+    
+}
